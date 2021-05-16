6b43320cf3cbf8934a28d11787ff03dd6076327c
Patryk Dabrowski <patrykdabrowski58@gmail.com>
2021-05-16 11:36:21 +0200

    Implement gyroscope page

--- lib/screens/gyroscope_page.dart
+++ lib/screens/gyroscope_page.dart
@@ -1,16 +1,58 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:sensors/sensors.dart';
 
+class GyroscopePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _GyroscopePage();
+}
 
-class GyroscopePage extends StatelessWidget {
+class _GyroscopePage extends State<GyroscopePage> {
+  List<double>? _gyroscopeValues;
+
+  List<StreamSubscription<dynamic>> _streamSubscriptions =
+      <StreamSubscription<dynamic>>[];
+
   @override
   Widget build(BuildContext context) {
+    final List<String>? gyroscope =
+    _gyroscopeValues?.map((double v) => v.toStringAsFixed(1)).toList();
+    final String? xValue = gyroscope?.elementAt(0);
+    final String? yValue = gyroscope?.elementAt(1);
+    final String? zValue = gyroscope?.elementAt(2);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Gyroscope'),
       ),
       body: Center(
-        child: Text('Gyroscope'),
-      ),
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text('Gyroscope'),
+          Text('x: $xValue, y: $yValue, z: $zValue'),
+        ],
+      )),
     );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    for (StreamSubscription<dynamic> subscription in _streamSubscriptions) {
+      subscription.cancel();
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _streamSubscriptions.add(gyroscopeEvents.listen((GyroscopeEvent event) {
+      setState(() {
+        _gyroscopeValues = <double>[event.x, event.y, event.z];
+      });
+    }));
   }
 }
